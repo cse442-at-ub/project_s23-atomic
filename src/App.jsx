b770dfda6e9c3b181194fdb5970606aa842ccd1d
karli-T <60751070+karli-T@users.noreply.github.com>
2023-03-11 15:53:20 -0500

    updated css for landing page text #26

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Landing from './components/Landing/Landing'
 function App() {
   return (
     <div className="App">
-     {/* <header className="App-header">
+     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,10 +22,10 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
 
       {/* <Signin /> */}
-      <ChooseHabits />
+      {/* <ChooseHabits /> */}
       {/* <Landing /> */}
 
     </div>
