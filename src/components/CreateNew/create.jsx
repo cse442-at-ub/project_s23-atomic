4b3a589138bc781b227d5c3aa31324f8b94ce675
Wren <42078212+Wrenmart@users.noreply.github.com>
2023-04-05 18:30:42 -0400

    Preparing to merge dev into this branch #31

--- src/components/CreateNew/create.jsx
+++ src/components/CreateNew/create.jsx
@@ -1,13 +1,10 @@
 import React from "react"
-import Navbar from "../../Navbar/Navbar"
+import Navbar from "../Navbar/Navbar"
 import './createpage.css'
 
 export default function create() {
     return (
     <div className="createContainer">
-    <p className="socialHabitsList">  </p>
-    <h2 className="socialHabits"></h2>
-    <h2 className="socialHabitsTitle">Social</h2>
     <Navbar/>
     </div>
     );
