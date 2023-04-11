575b5fab31da19bae2b282a4d1c8f1366273e89b
Wren <42078212+Wrenmart@users.noreply.github.com>
2023-04-10 23:00:25 -0400

    Successfully setting state with Title and Details #31

--- src/components/CreateNew/create.jsx
+++ src/components/CreateNew/create.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useContext } from 'react'
 import HabitContext from '../contexts/HabitContext';
@@ -11,6 +11,37 @@ import "./create.css"
 function Create() {
     const {user, good_habits, bad_habits,addGoodHabit,addBadHabit,sendHabits} = useContext(HabitContext);
 
+    // const test = {
+    //     "Sleep 6-8 Hours":
+    //         {"title":"Sleep 6-8 Hours",
+    //         "counter": 0,
+    //         "total": 1,
+    //         "details": "Getting enough sleep is vital to your health.",
+    //         "category": "Health"
+    //         // "Days":{7: 0, 6: 0, 5: 0, 4: 0, 3: 0, 2: 0, 1: 0, 0: {"date": current_date,"counter": 0} }
+    //         },
+
+    // }
+
+    const [habit, setHabit] = useState({
+        title: "",
+        counter: 0,
+        details: "",
+        category: ""
+    });
+    const setTitle = (event) => {
+        setHabit({ ...habit, title: event.target.value });
+    };
+    const setCounter = (event) => {
+        setHabit({ ...habit, counter: event.target.value });
+    };
+    const setDetails = (event) => {
+        setHabit({ ...habit, details: event.target.value });
+    };
+    const setCategory = (event) => {
+        setHabit({ ...habit, category: event.target.value });
+    };
+    
     return (
     <div id="create-container">
         <h2>Create New Habit</h2>
@@ -19,11 +50,11 @@ function Create() {
         </div>
         <div id="title">
             <h5>Title</h5>
-            <input type="text" />
+            <input onChange={setTitle} type="text" />
         </div>
         <div id="details">
             <h5>Details</h5>
-            <input type="text" />
+            <input onChange={setDetails} type="text" />
         </div>
         <div id="counter">
             <h5>Counter</h5>
