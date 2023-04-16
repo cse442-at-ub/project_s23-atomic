bf5dd6efcbb312a6b5d567c299c6de52857db2ec
argraca <60617633+argraca@users.noreply.github.com>
2023-04-15 22:21:47 -0400

    removed sensitive info again #35

--- PHP/connection.php
+++ PHP/connection.php
@@ -1,8 +1,8 @@
 <?php
 
 $dbhost = "oceanus.cse.buffalo.edu:3306";
-$dbuser = "argraca";
-$dbpass = "50301883";
+$dbuser = "";
+$dbpass = "";
 $dbname = "cse442_2023_spring_team_q_db";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
